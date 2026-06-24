303d388b2493ceba715a534050e42fd804b33101
Joshua Leal <118430438+joshnleal@users.noreply.github.com>
2026-06-24 16:04:13 +0200

    Create file structure and .html, .css, and .js and link all files together and test

---
+++ resources/js/script.js
@@ -0,0 +1,8 @@
+const sidebar = document.querySelector(".sidebar");
+
+let testItem = document.createElement("div");
+testItem.style.backgroundColor = "pink";
+testItem.style.height = "50%";
+testItem.style.width = "50%";
+
+sidebar.appendChild(testItem);
